cee73b8123e200add32e8e13926d0d3240a5eba9
eric <eric.chingg@gmail.com>
2023-09-23 10:34:42 -0400

    adding fruit search proj

---
+++ fruit search/script.js
@@ -0,0 +1,44 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+	let results = [];
+	// TODO
+	let inputVal = input.value;
+	
+	if (inputVal.length) {
+		results = fruit.filter((letter) => {
+			return letter.toLowerCase().includes(inputVal.toLowerCase())
+		});
+	}
+	return results;
+}
+
+function searchHandler(e) {
+	// TODO
+	let results = [];
+	let inputVal = input.value;
+
+	results = search(inputVal);
+	showSuggestions(results);
+}
+
+function showSuggestions(results, inputVal) {
+	// TODO
+	const display = results.map((list) => {
+		return '<li>'+ list + '</li>';
+	});	
+	
+	suggestions.innerHTML = display.join('');
+}
+
+function useSuggestion(e) {
+	// TODO
+	input.value = e.target.innerText;
+	suggestions.innerHTML = '';
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
