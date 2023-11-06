ae4f9c2ef0da9a50374447becbf313f61a866b50
eric <eric.chingg@gmail.com>
2023-11-05 22:13:16 -0500

    adding session in survey

--- flask/flask-survey/app.py
+++ flask/flask-survey/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request , redirect, flash
+from flask import Flask, render_template, request , redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension 
 from surveys import satisfaction_survey
 
@@ -8,14 +8,19 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
+user_answer = 'responses'
 
 @app.route('/')
 def start_page():
+
+  session[user_answer] = []
+
   return render_template('start.html', satisfaction_survey=satisfaction_survey)
 
 @app.route('/questions/<int:num>')
 def show_questions(num):
+
+  responses = session.get(user_answer)
 
   if (len(responses) != num):
     flash(f"Question number {num} is invalid.")
@@ -29,7 +34,9 @@ def show_questions(num):
 @app.route('/answer', methods=['POST'])
 def get_ans():
   choice = request.form['answer']
+  responses = session[user_answer]
   responses.append(choice)
+  session[user_answer] = responses
 
   if (len(responses) == len(satisfaction_survey.questions)):
     return redirect("/finish")
